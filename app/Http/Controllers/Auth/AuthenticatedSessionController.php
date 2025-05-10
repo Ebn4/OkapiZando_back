bdef54afa0d653e4ee88443537dce783e657fdf6
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-10 17:49:52 +0200

    fix : Reglage de probleme de l'api d'authentification

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
@@ -21,7 +20,6 @@ class AuthenticatedSessionController extends Controller
 
 
         return response()->json([
-            'message' => 'Login successful',
             'user' => $request->user(),
             'token' => $request->user()->createToken('auth_token')->plainTextToken,
         ]);
@@ -30,7 +28,7 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
 
@@ -38,6 +36,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return response()->noContent();
+        return response()->json([
+            'message' => "user has been logged out succesfully"
+        ], 200);
     }
 }
